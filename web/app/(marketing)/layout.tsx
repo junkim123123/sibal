e3a22118231e124cf43257749ac2632b6a515c02
junkim123123 <junkimfrom82@gmail.com>
2025-12-10 02:39:00 -0600

    킼

--- web/app/(marketing)/layout.tsx
+++ web/app/(marketing)/layout.tsx
@@ -1,6 +1,6 @@
 import { MainHeader } from '@/components/site/main-header';
 import { MainFooter } from '@/components/site/main-footer';
-import Script from 'next/script';
+// import Script from 'next/script';
 
 export default function MarketingLayout({
   children,
@@ -9,7 +9,8 @@ export default function MarketingLayout({
 }) {
   return (
     <div className="flex min-h-screen flex-col">
-      <Script src="https://gumroad.com/js/gumroad.js" strategy="lazyOnload" />
+      {/* 💥 Gumroad 스크립트 일시 중지: CSP 위반으로 인한 투명 오버레이 문제 해결 */}
+      {/* <Script src="https://gumroad.com/js/gumroad.js" strategy="lazyOnload" /> */}
       <MainHeader />
       <main className="flex-1">{children}</main>
       <MainFooter />
